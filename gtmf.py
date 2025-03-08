f8cfd892e61c77bee79c9517f3135b0575bed82d
Christopher Skorka <chris.skorka@gmail.com>
2025-03-08 12:51:45 +1100

    parser finds primary path from file content

--- gtmf.py
+++ gtmf.py
@@ -1,32 +1,182 @@
+# pyright: strict
+
+from abc import ABC, abstractmethod
 import os
 import json
 
 from pathlib import Path
 from datetime import datetime
 from argparse import ArgumentParser
+import sys
+from typing import Any, Generator, Self, Type
+from html.parser import HTMLParser
 
-from typing import Generator
+
+class MetadataParser(ABC):
+    def __init__(self, metadata_path: Path, primary_path: Path) -> None:
+        self.metadata_path = metadata_path
+        self.primary_path = primary_path
+
+    @classmethod
+    @abstractmethod
+    def get_compatible_suffixes(cls) -> list[str]:
+        pass
+
+    @classmethod
+    @abstractmethod
+    def create(cls, metadata_path: Path) -> None | Self:
+        pass
+
+    @abstractmethod
+    def apply_metadata_to_primary(self) -> bool:
+        pass
+
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__}({repr(self.metadata_path)}, {repr(self.primary_path)})'
 
 
-def fix_metadata(source_directory: Path, metadata_destination_directory: None | Path, metadata_suffix: str):
+class JsonParser(MetadataParser):
+    def __init__(self, metadata_path: Path, primary_path: Path, data: dict[str, Any]) -> None:
+        super().__init__(metadata_path, primary_path)
+        self.data = data
+
+    @classmethod
+    def get_compatible_suffixes(cls) -> list[str]:
+        return ['.json']
+
+    @classmethod
+    def create(cls, metadata_path: Path) -> None | Self:
+        try:
+            data: None | dict[str, Any] = json.loads(metadata_path.read_text())
+            if data == None:
+                return None
+
+            title = data.get('title', None)
+            if title is None:
+                return None
+
+            primary_path = metadata_path.parent.joinpath(title)
+            if primary_path.exists():
+                return cls(metadata_path, primary_path, data)
+
+            primary_path = primary_path.parent
+            if primary_path.exists():
+                return cls(metadata_path, primary_path, data)
+
+            return None
+
+        except Exception:
+            return None
+
+    def apply_metadata_to_primary(self) -> bool:
+        try:
+            if 'last_modified_by_any_user' in self.data:
+                timestamp = datetime.strptime(
+                    self.data['last_modified_by_any_user'],
+                    "%Y-%m-%dT%H:%M:%S.%fZ",
+                )
+                return self.apply_modify_timestamp(timestamp.timestamp())
+
+            if 'photoTakenTime' in self.data and 'timestamp' in self.data['photoTakenTime']:
+                timestamp = float(self.data['photoTakenTime']['timestamp'])
+                return self.apply_modify_timestamp(timestamp)
+
+            if 'date' in self.data and 'timestamp' in self.data['date']:
+                timestamp = float(self.data['date']['timestamp'])
+                return self.apply_modify_timestamp(timestamp)
+
+        except Exception:
+            return False
+        return False
+
+    def apply_modify_timestamp(self, timestamp: float) -> bool:
+        os.utime(self.primary_path, (timestamp, timestamp))
+        return True
+
+
+class CommentsHtmlParser(MetadataParser):
+
+    class TitleParser(HTMLParser):
+        def __init__(self) -> None:
+            super().__init__()
+            self.title: None | str = None
+            self.path: list[str] = []
+
+        def handle_starttag(self, tag: str, attrs: list[tuple[str, str | None]]):
+            self.path.append(tag)
+
+        def handle_endtag(self, tag: str):
+            self.path.pop()
+
+        def handle_data(self, data: str):
+            if self.path == ['html', 'title']:
+                self.title = data
+
+    @classmethod
+    def get_compatible_suffixes(cls) -> list[str]:
+        return ['.html']
+
+    @classmethod
+    def create(cls, metadata_path: Path) -> None | Self:
+        try:
+            parser = CommentsHtmlParser.TitleParser()
+            parser.feed(metadata_path.read_text())
+
+            title = parser.title
+            if title is None:
+                return None
+
+            primary_path = metadata_path.parent.joinpath(title)
+            if not primary_path.exists():
+                return None
+
+            return cls(metadata_path, primary_path)
+
+        except Exception:
+            return None
+
+    def apply_metadata_to_primary(self) -> bool:
+        return True
+
+
+metadata_parsers: list[Type[MetadataParser]] = [
+    JsonParser,
+    CommentsHtmlParser,
+]
+
+
+def fix_metadata(source_directory: Path, metadata_destination_directory: None | Path):
 
     for directory in get_depth_first_directories(source_directory):
 
-        child_items_by_name: dict[str, Path] = {item.name: item for item in directory.iterdir()}
-        primary_to_metadata_file_map: dict[Path, Path] = {}
+        child_items_by_name: dict[str, Path] = {
+            item.name: item
+            for item
+            in directory.iterdir()
+        }
 
-        for name, path in child_items_by_name.items():
-            if name.endswith(metadata_suffix):
-                primary_item_name = name[:-len(metadata_suffix)]
-                if primary_item_name in child_items_by_name:
-                    primary_to_metadata_file_map[child_items_by_name[primary_item_name]] = path
-        
-        for primary_item, metadata_file in primary_to_metadata_file_map.items():
-            apply_metadata(primary_item, metadata_file)
-            if metadata_destination_directory:
-                new_metadata_file = metadata_destination_directory / metadata_file.relative_to(source_directory)
-                os.makedirs(new_metadata_file.parent, exist_ok=True)
-                metadata_file.rename(new_metadata_file)
+        parsers: list[MetadataParser] = [
+            parser
+            for (metadata_name, metadata_path) in child_items_by_name.items()
+            for parser_cls in metadata_parsers
+            for suffix in parser_cls.get_compatible_suffixes()
+            if metadata_name.endswith(suffix)
+            if (parser := parser_cls.create(metadata_path)) is not None
+        ]
+
+        for metadata_parser in parsers:
+            try:
+                is_applied = metadata_parser.apply_metadata_to_primary()
+                if is_applied and metadata_destination_directory:
+                    metadata_parser.metadata_path = move_metadata_file(
+                        metadata_parser.metadata_path,
+                        source_directory,
+                        metadata_destination_directory,
+                    )
+                print(
+                    f'Fixed metadata for {metadata_parser.primary_path} with {metadata_parser.metadata_path}')
+            except Exception as e:
+                print(metadata_parser.metadata_path, repr(e), file=sys.stderr)
 
 
 def get_depth_first_directories(path: Path) -> Generator[Path, None, None]:
@@ -36,41 +186,51 @@ def get_depth_first_directories(path: Path) -> Generator[Path, None, None]:
         yield path
 
 
-def apply_metadata(primary_item: Path, metadata_file: Path):
-    metadata: dict = json.loads(metadata_file.read_text())
-    last_modified: str = metadata['last_modified_by_any_user']
-    modify_time = datetime.strptime(last_modified, "%Y-%m-%dT%H:%M:%S.%fZ")
-    os.utime(primary_item, (modify_time.timestamp(), modify_time.timestamp()))
+def move_metadata_file(
+    metadata_path: Path,
+    source_directory: Path,
+    metadata_destination_directory: Path,
+) -> Path:
+    new_metadata_path = Path.joinpath(
+        metadata_destination_directory,
+        metadata_path.relative_to(source_directory),
+    )
+    os.makedirs(new_metadata_path.parent, exist_ok=True)
+    metadata_path.rename(new_metadata_path)
+    return new_metadata_path
 
-
-def rebase_pah(path: Path, old_base: Path, new_base: Path) -> Path:
-    return new_base / path.relative_to(old_base)
 
 def main():
     parser = ArgumentParser(
         prog='Google takeout Metadata Fixer',
-       description=''
+        description=''
     )
     parser.add_argument(
         'source_directory',
-       help='path to root directory of a Google Takeout',
+        help='path to root directory of a Google Takeout',
     )
     parser.add_argument(
         'metadata_destination_directory',
         help='optional path to root directory where the metadata will be moved to',
-       nargs='?',
-    )
-    parser.add_argument(
-        '-s',
-        '--suffix',
-        default='-info.json',
-        help='The suffix appended to the primary file name to identify the metadata file',
+        nargs='?',
     )
     parsed_args = parser.parse_args()
-    source_directory = Path(parsed_args.source_directory)
-    metadata_destination_directory = Path(parsed_args.metadata_destination_directory) if parsed_args.metadata_destination_directory else None
-    suffix = parsed_args.suffix
-    fix_metadata(source_directory, metadata_destination_directory, suffix)
+    
+    # parse and check source_directory
+    source_directory: Path = Path(parsed_args.source_directory)
+    source_directory.mkdir(parents=True, exist_ok=True)
+    assert source_directory.is_dir(), 'source_directory must be a directory'
+    
+    # parse and check metadata_destination_directory
+    metadata_destination_directory: None | Path = None
+    if parsed_args.metadata_destination_directory is not None:
+        metadata_destination_directory = Path(parsed_args.metadata_destination_directory)
+        metadata_destination_directory.mkdir(parents=True, exist_ok=True)
+        assert metadata_destination_directory.is_dir(), 'metadata_destination_directory must be a directory'    
+
+    # fix metadata
+    fix_metadata(source_directory, metadata_destination_directory)
+
 
 if __name__ == '__main__':
     main()
